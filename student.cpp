74b2f78e09e00c2438ebae24c22df3a28f64422d
diacob-stack <diacob@ucsb.edu>
2026-04-16 00:10:03 -0700

    valgrind maybe fixed

--- student.cpp
+++ student.cpp
@@ -21,8 +21,9 @@ void Student::setPerm(const int permNumber) {
 }
 
 void Student::setName(const char * const name) {
-  this->name = new char[strlen(name)+1];
-  strcpy(this->name,name);
+  	delete[] this->name;
+	this->name = new char[strlen(name)+1];
+  	strcpy(this->name,name);
 }
 
 
