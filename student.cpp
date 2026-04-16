20749f6f19fde869b50e9ab9fd489a64b5411ffe
diacob-stack <diacob@ucsb.edu>
2026-04-15 23:52:49 -0700

    first file working

--- student.cpp
+++ student.cpp
@@ -1,35 +1,39 @@
 #include "student.h"
 #include <string>
 #include <cstring>
+using namespace std;
 
 Student::Student(const char * const name, int perm) {
   this->setName("another stub");
 }
 
 int Student::getPerm() const {
-  return -42;
+  return perm;
 }
 
 const char * const Student::getName() const {
-  return "stub";
+  return name;
 }
 
 void Student::setPerm(const int permNumber) {
+	perm = permNumber;
 }
 
 void Student::setName(const char * const name) {
-  this->name = new char[strlen("stub")+1];
-  strcpy(this->name,"stub");
+  this->name = new char[strlen(name)+1];
+  strcpy(this->name,name);
 }
 
 
 Student::Student(const Student &orig) {
-  this->setName("yet another stub");
-  this->setPerm(-42);
+  this->setName(orig.name);
+  this->setPerm(orig.perm);
 }
 
 Student::~Student() {
-
+	delete[] name;
+	name = nullptr;
+	perm = 0;
 }
 
 Student & Student::operator=(const Student &right) {
@@ -43,6 +47,8 @@ Student & Student::operator=(const Student &right) {
   // TODO... Here is where there is code missing that you need to 
   // fill in...
 
+	this->setName(right.name);
+	this->setPerm(right.perm);
 
   // KEEP THE CODE BELOW THIS LINE
   // Overloaded = should end with this line, despite what the textbook says.
@@ -51,6 +57,6 @@ Student & Student::operator=(const Student &right) {
 }
 
 std::string Student::toString() const {
-  return "tostring stub";
+  return "[" + string(name) + "," + to_string(perm) + "]";
 }
 
